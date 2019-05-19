f53436afdf82b4ba730f9f3a9978dcaf5fe3a0b7
pearjam <samgriffiths7@gmail.com>
2019-05-19 20:26:30 +0200

    Lesson 03 - Login UI added

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -78,15 +78,15 @@ public final class R {
         public static final int gone = 0x7f08004e;
         public static final int invisible = 0x7f080058;
         public static final int left = 0x7f08005d;
-        public static final int packed = 0x7f080078;
-        public static final int parent = 0x7f08007a;
-        public static final int percent = 0x7f08007d;
-        public static final int right = 0x7f080082;
-        public static final int spread = 0x7f0800a6;
-        public static final int spread_inside = 0x7f0800a7;
-        public static final int start = 0x7f0800ac;
-        public static final int top = 0x7f0800c2;
-        public static final int wrap = 0x7f0800d4;
+        public static final int packed = 0x7f08007c;
+        public static final int parent = 0x7f08007e;
+        public static final int percent = 0x7f080081;
+        public static final int right = 0x7f080086;
+        public static final int spread = 0x7f0800aa;
+        public static final int spread_inside = 0x7f0800ab;
+        public static final int start = 0x7f0800b0;
+        public static final int top = 0x7f0800c7;
+        public static final int wrap = 0x7f0800d9;
     }
     public static final class styleable {
         private styleable() {}
